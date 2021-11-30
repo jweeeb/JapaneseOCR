3f5cb1bf555ea85eba318350fa173f9f722e0826
jweeeb <jweb333@gmail.com>
2021-11-30 16:14:56 -0500

    initial commit

---
+++ JapaneseOCR/Controls/MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using IronOcr;
+using JapaneseOCR.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Point = System.Drawing.Point;
+
+namespace JapaneseOCR
+{
+
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window, INotifyPropertyChanged
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+      private Point _startSelection;
+      private Point _endSelection;
+      private bool _isClipping;
+      private List<Window> openWindows;
+
+      private string _ocrText;
+      public string OCRText
+      {
+         get
+         {
+            return _ocrText;
+         }
+         set
+         {
+            if (value != _ocrText)
+            {
+               _ocrText = value;
+               OnPropertyChanged("OCRText");
+            }
+         }
+      }
+
+      public MainWindow()
+      {
+         InitializeComponent();
+         this.DataContext = this;
+         this.openWindows = new List<Window>();
+      }
+
+      private void ClipAndTranslate()
+      {
+         // close the overlay so as not to effect the colors of the pixels that we are clipping
+         DisableClip();
+         
+         // clip rectangle from screen pixels, currently must be a rectangle starting in upper left to bottom right
+         // TODO: add logic to clip from bottom right to top left
+         int xdiff = (int)Math.Abs(this._endSelection.X - this._startSelection.X);
+         int ydiff = (int)Math.Abs(this._endSelection.Y - this._startSelection.Y);
+         System.Drawing.Rectangle bounds = new System.Drawing.Rectangle((int)this._startSelection.X, (int)this._startSelection.Y, xdiff, ydiff);
+         System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(xdiff, ydiff);
+         using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(bitmap))
+         {
+            g.CopyFromScreen((int)this._startSelection.X, (int)this._startSelection.Y, 0, 0, bounds.Size);
+         }
+         BitmapData bitmapData = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, bitmap.PixelFormat);
+         BitmapSource source = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+         System.Windows.Clipboard.SetImage(source);
+         bitmap.UnlockBits(bitmapData);
+         this.clipboardImage.Source = source;
+         
+         // parse image to japanese text
+         IronTesseract ocr = new IronTesseract();
+         ocr.Language = OcrLanguage.Japanese;
+         ocr.Configuration.TesseractVariables["preserve_interword_spaces"] = true;
+         OcrResult result = ocr.Read(bitmap);
+
+         // strips whitespace and new lines
+         // japanese doesnt need whitespaces and the ocr will insert blank lines based on space from the image
+         string resultText = Regex.Replace(result.Text, @"\s+", string.Empty);
+         this.OCRText = resultText;
+         Uri uri = new Uri($"https://jisho.org/search/{resultText}", UriKind.Absolute);
+         this.browser.Navigate(uri);
+      }
+
+      protected void OnPropertyChanged(string propertyName)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      }
+
+
+
+      protected override void OnClosing(CancelEventArgs e)
+      {
+         NativeMethods.ClearHook();
+         base.OnClosing(e);
+      }
+
+      private void startClipButton_Click(object sender, RoutedEventArgs e)
+      {
+         foreach(Screen screen in Screen.AllScreens)
+         {
+            Window window = new Window();
+            window.Left = screen.WorkingArea.Left;
+            window.Top = screen.WorkingArea.Top;
+            window.Width = screen.WorkingArea.Width;
+            window.Height = screen.WorkingArea.Height;
+            window.Topmost = true;
+            window.AllowsTransparency = true;
+            window.WindowStyle = WindowStyle.None;
+            window.Background = new SolidColorBrush(System.Windows.Media.Color.FromArgb(20, 255, 255, 255));
+            Canvas canvas = new Canvas();
+            canvas.Width = window.Width;
+            canvas.Height = window.Height;
+            window.Content = canvas;
+            openWindows.Add(window);
+            window.Show();
+         }
+
+         NativeMethods.MouseHandle = NativeMouseHandle;
+         NativeMethods.KeyboardHandle = NativeKeyboardHandle;
+      }
+
+      public void NativeMouseHandle(NativeMethods.MouseEventModel info)
+      {
+         switch (info.EventType)
+         {
+            case NativeMethods.MouseMessage.WM_LBUTTONDOWN:
+               this._startSelection = info.Coordinates;
+               this._isClipping = true;
+               break;
+            case NativeMethods.MouseMessage.WM_LBUTTONUP:
+               if (this._isClipping)
+               {
+                  this._endSelection = info.Coordinates;
+                  if (Math.Abs(this._endSelection.X - this._startSelection.X) > 10 && Math.Abs(this._endSelection.Y - this._startSelection.Y) > 10)
+                  {
+                     this.ClipAndTranslate();
+                  }
+               }
+               this._isClipping = false;
+               break;
+            case NativeMethods.MouseMessage.WM_MOUSEMOVE:
+               // draw highlight rect
+               break;
+         }
+      }
+
+      public void NativeKeyboardHandle(NativeMethods.KeyboardEventModel info)
+      {
+         if(info.EventType == NativeMethods.KeyboardMessage.WM_KEYDOWN && info.Key == Keys.Escape)
+         {
+            DisableClip();
+         }
+      }
+
+      private void DisableClip()
+      {
+         foreach (Window window in openWindows)
+         {
+            window.Close();
+         }
+         openWindows.Clear();
+         NativeMethods.MouseHandle = null;
+         NativeMethods.KeyboardHandle = null;
+         this._isClipping = false;
+      }
+   }
+}
---
+++ JapaneseOCR/Helpers/NativeMethods.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JapaneseOCR.Helpers
+{
+   public static class NativeMethods
+   {
+      public class MouseEventModel
+      {
+         public MouseMessage EventType { get; set; }
+         public Point Coordinates { get; set; }
+      }
+
+      public class KeyboardEventModel
+      {
+         public KeyboardMessage EventType { get; set; }
+         public Keys Key { get; set; }
+      }
+
+      public enum HookType : int
+      {
+         WH_KEYBOARD = 2,
+         WH_MOUSE = 7,
+         WH_KEYBOARD_LL = 13,
+         WH_MOUSE_LL = 14
+      }
+
+      public enum MouseMessage
+      {
+         WM_MOUSEMOVE = 0x0200,
+         WM_LBUTTONDOWN = 0x0201,
+         WM_LBUTTONUP = 0x0202,
+         WM_LBUTTONDBLCLK = 0x0203,
+         WM_RBUTTONDOWN = 0x0204,
+         WM_RBUTTONUP = 0x0205,
+         WM_RBUTTONDBLCLK = 0x0206,
+         WM_MBUTTONDOWN = 0x0207,
+         WM_MBUTTONUP = 0x0208,
+         WM_MBUTTONDBLCLK = 0x0209,
+
+         WM_MOUSEWHEEL = 0x020A,
+         WM_MOUSEHWHEEL = 0x020E,
+
+         WM_NCMOUSEMOVE = 0x00A0,
+         WM_NCLBUTTONDOWN = 0x00A1,
+         WM_NCLBUTTONUP = 0x00A2,
+         WM_NCLBUTTONDBLCLK = 0x00A3,
+         WM_NCRBUTTONDOWN = 0x00A4,
+         WM_NCRBUTTONUP = 0x00A5,
+         WM_NCRBUTTONDBLCLK = 0x00A6,
+         WM_NCMBUTTONDOWN = 0x00A7,
+         WM_NCMBUTTONUP = 0x00A8,
+         WM_NCMBUTTONDBLCLK = 0x00A9
+      }
+
+      public enum KeyboardMessage
+      {
+         WM_KEYDOWN = 0x0100,
+      }
+
+      [DllImport("user32.dll")]
+      [return: MarshalAs(UnmanagedType.Bool)]
+      static extern bool GetCursorPos(ref Point lpPoint);
+
+      [DllImport("user32.dll", CharSet = CharSet.Auto)]
+      public static extern IntPtr SetWindowsHookEx(HookType hookType, HookProc callback, IntPtr hMod, uint dwThreadId);
+      [DllImport("user32.dll", CharSet = CharSet.Auto)]
+      public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+      [DllImport("user32.dll", CharSet = CharSet.Auto)]
+      public static extern int CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+      [DllImport("User32.dll")]
+      public static extern IntPtr GetDC(IntPtr hwnd);
+      [DllImport("User32.dll")]
+      public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+
+      public delegate int HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+      public delegate void MouseEventDelegate(MouseEventModel info);
+      public static MouseEventDelegate MouseHandle;
+      public delegate void KeyboardEventDelegate(KeyboardEventModel info);
+      public static KeyboardEventDelegate KeyboardHandle;
+
+      private static HookProc _globalLlMouseHookCallback;
+      private static HookProc _globalLlKeyboardHookCallback;
+      private static IntPtr _hGlobalLlMouseHook;
+      private static IntPtr _hGlobalLlKeyboardHook;
+
+      static NativeMethods()
+      {
+         _hGlobalLlMouseHook = IntPtr.Zero;
+         _hGlobalLlKeyboardHook = IntPtr.Zero;
+         _globalLlMouseHookCallback = LowLevelMouseProc;
+         _globalLlKeyboardHookCallback = LowLevelKeyboardProc;
+         SetUpHook();
+      }
+
+      private static void SetUpHook()
+      {
+         _hGlobalLlMouseHook = SetWindowsHookEx(HookType.WH_MOUSE_LL, _globalLlMouseHookCallback, Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]), 0);
+
+         if (_hGlobalLlMouseHook == IntPtr.Zero)
+         {
+            throw new Win32Exception("Unable to set MouseHook");
+         }
+
+         _hGlobalLlKeyboardHook = SetWindowsHookEx(HookType.WH_KEYBOARD_LL, _globalLlKeyboardHookCallback, Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]), 0);
+
+         if (_hGlobalLlKeyboardHook == IntPtr.Zero)
+         {
+            throw new Win32Exception("Unable to set KeyboardHook");
+         }
+      }
+
+      public static int LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam)
+      {
+         if (nCode >= 0)
+         {
+            MouseEventModel eventModel = new MouseEventModel();
+            // Get the mouse WM from the wParam parameter
+            var wmMouse = (MouseMessage)wParam;
+            eventModel.EventType = wmMouse;
+            Point p = new Point();
+            GetCursorPos(ref p);
+            eventModel.Coordinates = p;
+            MouseHandle?.Invoke(eventModel);
+
+         }
+
+         // Pass the hook information to the next hook procedure in chain
+         return CallNextHookEx(_hGlobalLlMouseHook, nCode, wParam, lParam);
+      }
+
+
+      public static int LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam)
+      {
+         if (nCode >= 0)
+         {
+            KeyboardEventModel eventModel = new KeyboardEventModel();
+            int vkCode = Marshal.ReadInt32(lParam);
+            eventModel.EventType = (KeyboardMessage)wParam;
+            eventModel.Key = (Keys)vkCode;
+            KeyboardHandle?.Invoke(eventModel);
+         }
+
+         // Pass the hook information to the next hook procedure in chain
+         return CallNextHookEx(_hGlobalLlKeyboardHook, nCode, wParam, lParam);
+      }
+
+      public static void ClearHook()
+      {
+         if (_hGlobalLlMouseHook != IntPtr.Zero)
+         {
+            // Unhook the low-level mouse hook
+            if (!UnhookWindowsHookEx(_hGlobalLlMouseHook))
+            {
+               throw new Win32Exception("Unable to clear MouseHook");
+            }
+            _hGlobalLlMouseHook = IntPtr.Zero;
+         }
+         if (_hGlobalLlKeyboardHook != IntPtr.Zero)
+         {
+            // Unhook the low-level mouse hook
+            if (!UnhookWindowsHookEx(_hGlobalLlKeyboardHook))
+            {
+               throw new Win32Exception("Unable to clear KeyboardHook");
+            }
+            _hGlobalLlKeyboardHook = IntPtr.Zero;
+         }
+      }
+   }
+}
